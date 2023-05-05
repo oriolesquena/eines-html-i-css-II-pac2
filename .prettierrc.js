46670f2560af024414e3cec354828123f2f63921
[Oriol Esquena Calm] <oesquena@uoc.edu>
2023-05-05 18:04:21 +0200

    addapting repo

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  trailingComma: 'es5',
+  printWidth: 80,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  bracketSpacing: false,
+  arrowParens: 'avoid',
+  useTabs: false,
+  proseWrap: 'always',
+};
--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -10,6 +10,8 @@
  * Write any other JavaScript below
  */
 
+import * as bootstrap from 'bootstrap';
+
 +( function() {
   const university = "UOC";
   console.log(`Hello, ${university}!`);
